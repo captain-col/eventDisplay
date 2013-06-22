e1e91d79f29da3f3122e9c6fbd0e901392efed51
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-06-21 21:49:07 -0400

    Show a TReconCluster as a elliptical tube.

--- src/TFitChangeHandler.cxx
+++ src/TFitChangeHandler.cxx
@@ -10,11 +10,19 @@
 #include <THandle.hxx>
 
 #include <TGeoManager.h>
+#include <TGeoShape.h>
+#include <TGeoEltu.h>
+#include <TGeoMatrix.h>
+
+#include <TVectorF.h>
+#include <TMatrixF.h>
+
 #include <TGButton.h>
 #include <TGListBox.h>
 #include <TCollection.h>
 
 #include <TEveManager.h>
+#include <TEveGeoShape.h>
 #include <TEveLine.h>
 
 #include <sstream>
@@ -24,6 +32,11 @@ CP::TFitChangeHandler::TFitChangeHandler() {
     fHitList->SetMainColor(kYellow);
     fHitList->SetMainAlpha(1.0);
     gEve->AddElement(fHitList);
+
+    fFitList = new TEveElementList("FitList","Reconstructed Objects");
+    fFitList->SetMainColor(kGreen);
+    fFitList->SetMainAlpha(0.5);
+    gEve->AddElement(fFitList);
 }
 
 CP::TFitChangeHandler::~TFitChangeHandler() {
@@ -32,6 +45,7 @@ CP::TFitChangeHandler::~TFitChangeHandler() {
 void CP::TFitChangeHandler::Apply() {
 
     fHitList->DestroyElements();
+    fFitList->DestroyElements();
 
     if (!CP::TEventDisplay::Get().GUI().GetShowFitsButton()->IsOn()) {
         CaptLog("Fits display disabled");
@@ -47,8 +61,6 @@ void CP::TFitChangeHandler::Apply() {
     CP::TEventDisplay::Get().GUI().GetResultsList()
         ->GetSelectedEntries(&selected);
 
-    CP::TShowDriftHits showDrift(1.6*unit::mm/unit::microsecond);
-
     // Iterate through the list of selected entries.
     TIter next(&selected);
     TGLBEntry* lbEntry;
@@ -56,19 +68,128 @@ void CP::TFitChangeHandler::Apply() {
         std::string objName(lbEntry->GetTitle());
         CP::THandle<CP::TReconObjectContainer> objects 
             = event->Get<CP::TReconObjectContainer>(objName.c_str());
-        if (!objects) {
-            CaptError("No object container " << objName);
-            continue;
-        }            
-        std::cout << " objects " << objects->GetName() << std::endl;
-        for (CP::TReconObjectContainer::iterator obj = objects->begin();
-             obj != objects->end(); ++obj) {
-            std::cout << "    object " << (*obj)->GetTitle() << std::endl;
-            std::cout << "      hits " << (*obj)->GetHits()->size() 
-                      << std::endl;
-            double t0 = (*obj)->GetHits()->front()->GetTime();
-            showDrift(fHitList, *(*obj)->GetHits(), t0);
-        }
+        ShowReconObjects(objects);
     }
 
+}
+
+void CP::TFitChangeHandler::ShowReconCluster(
+    CP::THandle<CP::TReconCluster> obj) {
+    if (!obj) return;
+    CaptLog("Cluster " << obj->GetName());
+
+    TEveGeoShape *clusterShape = new TEveGeoShape("cluster");
+    
+    clusterShape->SetMainColor(kRed);
+    clusterShape->SetMainTransparency(80);
+
+    // Find the orientation of the cluster from the eigen vectors.
+    TVectorF eigenValues;
+    TMatrixF eigenVectors(obj->GetMoments().EigenVectors(eigenValues));
+    TMatrixD eigenDirs(eigenVectors);
+
+    // Long axis goes on Z
+    eigenDirs(0,2) = eigenVectors(0,0);
+    eigenDirs(1,2) = eigenVectors(1,0);
+    eigenDirs(2,2) = eigenVectors(2,0);
+    double len = std::sqrt(eigenValues(0));
+
+    // Major axis goes on X
+    eigenDirs(0,0) = eigenVectors(0,1);
+    eigenDirs(1,0) = eigenVectors(1,1);
+    eigenDirs(2,0) = eigenVectors(2,1);
+    double major = std::sqrt(eigenValues(1));
+
+    // Minor axis goes on Y
+    eigenDirs(0,1) = eigenVectors(0,2);
+    eigenDirs(1,1) = eigenVectors(1,2);
+    eigenDirs(2,1) = eigenVectors(2,2);
+    double minor = std::sqrt(eigenValues(2));
+        
+    // Create the rotation matrix.
+    TGeoRotation rot;
+    rot.SetMatrix(eigenDirs.GetMatrixArray());
+
+    // Set the translation
+    TGeoTranslation trans(obj->GetPosition().X(),
+                          obj->GetPosition().Y(),
+                          obj->GetPosition().Z());
+    
+    // Finally set the transform for the object.
+    TGeoCombiTrans rotTrans(trans,rot);
+    clusterShape->SetTransMatrix(rotTrans);
+
+    // Create the shape to display.  This has to play some fancy footsie to
+    // get the gGeoManager memory management right.  It first saves the
+    // current manager, then gets an internal geometry manager used by
+    // TEveGeoShape, and then resets the old manager once the shape is
+    // created.  You gotta love global variables...
+    TGeoManager* saveGeom = gGeoManager;
+    gGeoManager = clusterShape->GetGeoMangeur();
+    TGeoShape* geoShape = new TGeoEltu(major,minor,len);
+    clusterShape->SetShape(geoShape);
+    gGeoManager = saveGeom;
+   
+    fFitList->AddElement(clusterShape);
+    
+    // Draw the hits.
+    CP::TShowDriftHits showDrift;
+    showDrift(fHitList, *(obj->GetHits()), obj->GetPosition().T());
+}
+
+void CP::TFitChangeHandler::ShowReconShower(
+    CP::THandle<CP::TReconShower> obj) {
+    if (!obj) return;
+}
+
+void CP::TFitChangeHandler::ShowReconTrack(
+    CP::THandle<CP::TReconTrack> obj) {
+    if (!obj) return;
+}
+
+void CP::TFitChangeHandler::ShowReconPID(
+    CP::THandle<CP::TReconPID> obj) {
+    if (!obj) return;
+}
+
+void CP::TFitChangeHandler::ShowReconVertex(
+    CP::THandle<CP::TReconVertex> obj) {
+    if (!obj) return;
+}
+
+void CP::TFitChangeHandler::ShowReconObjects(
+    CP::THandle<CP::TReconObjectContainer> objects) {
+    if (!objects) return;
+    CaptLog("Show Objects in " << objects->GetName());
+    CP::TCaptLog::IncreaseIndentation();
+    for (CP::TReconObjectContainer::iterator obj = objects->begin();
+         obj != objects->end(); ++obj) {
+        CP::THandle<CP::TReconCluster> cluster = *obj;
+        if (cluster) {
+            ShowReconCluster(cluster);
+            continue;
+        }
+        CP::THandle<CP::TReconShower> shower = *obj;
+        if (shower) {
+            ShowReconShower(shower);
+            continue;
+        }
+        CP::THandle<CP::TReconTrack> track = *obj;
+        if (track) {
+            ShowReconTrack(track);
+            continue;
+        }
+        CP::THandle<CP::TReconPID> pid = *obj;
+        if (pid) {
+            ShowReconPID(pid);
+            continue;
+        }
+        CP::THandle<CP::TReconVertex> vertex = *obj;
+        if (vertex) {
+            ShowReconVertex(vertex);
+            continue;
+        }
+
+    }
+    CP::TCaptLog::DecreaseIndentation();
 }
--- src/TFitChangeHandler.hxx
+++ src/TFitChangeHandler.hxx
@@ -3,6 +3,14 @@
 
 #include "TVEventChangeHandler.hxx"
 
+#include <TReconBase.hxx>
+#include <TReconCluster.hxx>
+#include <TReconShower.hxx>
+#include <TReconTrack.hxx>
+#include <TReconPID.hxx>
+#include <TReconVertex.hxx>
+#include <THandle.hxx>
+
 #include <string>
 
 namespace CP {
@@ -14,28 +22,38 @@ class TEveElementList;
 /// Handle drawing the TAlgorithmResults saved in the event.
 class CP::TFitChangeHandler: public TVEventChangeHandler {
 public:
-    typedef std::vector<std::string> FitList;
+
     TFitChangeHandler();
     ~TFitChangeHandler();
     
     /// Draw fit information into the current scene.
     virtual void Apply();
 
-    /// Add a fit to the list of fits to be displayed.
-    void AddFit(std::string);
-
-    /// Clear the list of fits to be displayed.
-    void ClearFits();
-
 private:
 
-    /// A list of TAlgorithmResult names to be displayed.  If the result isn't
-    /// available, then it is skipped.
-    FitList fFitList;
+    /// A method to draw a TReconCluster.
+    void ShowReconCluster(const CP::THandle<CP::TReconCluster> obj);
+    
+    /// A method to draw a TReconShower.
+    void ShowReconShower(const CP::THandle<CP::TReconShower> obj);
+
+    /// A method to draw a TReconTrack.
+    void ShowReconTrack(const CP::THandle<CP::TReconTrack> obj);
+
+    /// A method to draw a TReconPID
+    void ShowReconPID(const CP::THandle<CP::TReconPID> obj);
+
+    /// A method to draw a TReconVertex
+    void ShowReconVertex(const CP::THandle<CP::TReconVertex> obj);
+
+    /// A method to draw a TReconObjectContainer
+    void ShowReconObjects(const CP::THandle<CP::TReconObjectContainer> obj);
+
+    /// The reconstruction objects to draw in the event.
+    TEveElementList* fFitList;
 
     /// The hits to draw in the event.
     TEveElementList* fHitList;
 
 };
-
 #endif
