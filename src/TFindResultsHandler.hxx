a4a115da7c761bdc69fc4043dac6c48da75fbf68
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-06-21 21:48:47 -0400

    Remove the unused list of fits to show.  This is handled through the GUI.

--- src/TFindResultsHandler.hxx
+++ src/TFindResultsHandler.hxx
@@ -15,18 +15,11 @@ class TEveElementList;
 /// GUI so they can be selected.
 class CP::TFindResultsHandler: public TVEventChangeHandler {
 public:
-    typedef std::vector<std::string> FitList;
     TFindResultsHandler();
     ~TFindResultsHandler();
     
     /// Draw fit information into the current scene.
     virtual void Apply();
-
-    /// Add a fit to the list of fits to be displayed.
-    void AddFit(std::string);
-
-    /// Clear the list of fits to be displayed.
-    void ClearFits();
 
 };
 
