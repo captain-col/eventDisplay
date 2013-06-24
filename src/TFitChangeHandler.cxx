dc5c9d6471aa0e01e2d3cf9d116ca9e1ef6817b9
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-06-24 17:18:08 -0400

    Add a private function to make it easy to show measurements.

--- src/TFitChangeHandler.cxx
+++ src/TFitChangeHandler.cxx
@@ -8,6 +8,7 @@
 #include <TEventFolder.hxx>
 #include <HEPUnits.hxx>
 #include <THandle.hxx>
+#include <TUnitsTable.hxx>
 
 #include <TGeoManager.h>
 #include <TGeoShape.h>
@@ -26,6 +27,77 @@
 #include <TEveLine.h>
 
 #include <sstream>
+
+namespace {
+    std::string ShowMeasurement(double val, double sig, std::string type) {
+        CP::TUnitsTable& u = CP::TUnitsTable::Get();
+        std::ostringstream measure;
+        if (type == "length") {
+            measure << u.ConvertLength(val);
+            if (CP::TCorrValues::IsFree(sig)) measure << " (free)";
+            else if (CP::TCorrValues::IsFixed(sig)) measure << " (fixed)";
+            else measure << "+-" << u.ConvertLength(sig);
+        }
+        else if (type == "time") {
+            measure << u.ConvertTime(val);
+            if (CP::TCorrValues::IsFree(sig)) measure << " (free)";
+            else if (CP::TCorrValues::IsFixed(sig)) measure << " (fixed)";
+            else measure << "+-" << u.ConvertTime(sig);
+        }
+        else if (type == "direction") {
+            measure << std::fixed;
+            measure << std::setprecision(3);
+            measure << val;
+            if (sig > 0) {
+                if (CP::TCorrValues::IsFree(sig)) measure << " (free)";
+                else if (CP::TCorrValues::IsFixed(sig)) measure << " (fixed)";
+                else measure << "+-" << sig;
+            }
+        }
+        else if (type == "angle") {
+            measure << std::fixed;
+            measure << std::setprecision(1);
+            measure << val/unit::degree;
+            if (sig<0) measure << " deg";
+            else if (CP::TCorrValues::IsFree(sig)) measure << " deg (free)";
+            else if (CP::TCorrValues::IsFixed(sig)) measure << " deg (fixed)";
+            else measure << "+-" << sig/unit::degree << " deg";
+        }
+        else if (type == "pe") {
+            measure << std::fixed;
+            measure << std::setprecision(1);
+            measure << val;
+            if (sig<0) measure << " pe";
+            else if (CP::TCorrValues::IsFree(sig)) measure << " pe (free)";
+            else if (CP::TCorrValues::IsFixed(sig)) measure << " pe (fixed)";
+            else measure << "+-" << sig << " pe";
+        }
+        else if (type == "momentum") {
+            measure << std::fixed;
+            measure << std::setprecision(1);
+            measure << val/unit::MeV << " MeV/c";
+            if (sig<0) measure << " MeV/c";
+            else if (CP::TCorrValues::IsFree(sig)) measure << " MeV/c (free)";
+            else if (CP::TCorrValues::IsFixed(sig)) measure << " MeV/c (fixed)";
+            else measure << "+-" << sig << " MeV/c";
+        }
+        else if (type == "charge") {
+            measure << std::fixed;
+            measure << std::setprecision(1);
+            measure << val;
+            if (sig<0) measure << " e";
+            else if (CP::TCorrValues::IsFree(sig)) measure << " e (free)";
+            else if (CP::TCorrValues::IsFixed(sig)) measure << " e (fixed)";
+            else measure << "+-" << sig << " e";
+        }
+        else {
+            CaptError("UNKNOWN TYPE <" << type << ">");
+            exit(1);
+        }
+
+        return measure.str();
+    }
+};
 
 CP::TFitChangeHandler::TFitChangeHandler() {
     fHitList = new TEveElementList("HitList","Reconstructed 3D Hits");
@@ -76,12 +148,23 @@ void CP::TFitChangeHandler::Apply() {
 void CP::TFitChangeHandler::ShowReconCluster(
     CP::THandle<CP::TReconCluster> obj) {
     if (!obj) return;
-    CaptLog("Cluster " << obj->GetName());
 
-    TEveGeoShape *clusterShape = new TEveGeoShape("cluster");
-    
-    clusterShape->SetMainColor(kRed);
-    clusterShape->SetMainTransparency(80);
+    CP::THandle<CP::TClusterState> state = obj->GetState();
+    if (!state) {
+        CaptError("TClusterState missing!");
+        return;
+    }
+    TLorentzVector var = state->GetPositionVariance();
+    TLorentzVector pos = state->GetPosition();
+
+    CaptLog("Cluster @ " 
+            << ShowMeasurement(pos.X(),std::sqrt(var.X()),"length")
+            << ", " << ShowMeasurement(pos.Y(),std::sqrt(var.Y()),"length")
+            << ", " << ShowMeasurement(pos.Z(),std::sqrt(var.Z()),"length"));
+
+    CP::TCaptLog::IncreaseIndentation();
+
+    CaptLog("Time " << ShowMeasurement(pos.T(),std::sqrt(var.T()),"time"));
 
     // Find the orientation of the cluster from the eigen vectors.
     TVectorF eigenValues;
@@ -105,6 +188,31 @@ void CP::TFitChangeHandler::ShowReconCluster(
     eigenDirs(1,1) = eigenVectors(1,2);
     eigenDirs(2,1) = eigenVectors(2,2);
     double minor = std::sqrt(eigenValues(2));
+
+    CaptLog("Long Axis (" << ShowMeasurement(len,-1,"length") << ")"
+            << " (" << ShowMeasurement(eigenVectors(0,0),-1,"direction")
+            << ", " << ShowMeasurement(eigenVectors(1,0),-1,"direction")
+            << ", " << ShowMeasurement(eigenVectors(2,0),-1,"direction") 
+            << ")");
+
+    CaptLog("Major     (" << ShowMeasurement(major,-1,"length") << ")"
+            << " (" << ShowMeasurement(eigenVectors(0,1),-1,"direction")
+            << ", " << ShowMeasurement(eigenVectors(1,1),-1,"direction")
+            << ", " << ShowMeasurement(eigenVectors(2,1),-1,"direction") 
+            << ")");
+
+    CaptLog("Minor     (" << ShowMeasurement(minor,-1,"length") << ")"
+            << " (" << ShowMeasurement(eigenVectors(0,2),-1,"direction")
+            << ", " << ShowMeasurement(eigenVectors(1,2),-1,"direction")
+            << ", " << ShowMeasurement(eigenVectors(2,2),-1,"direction") 
+            << ")");
+
+    CP::TCaptLog::DecreaseIndentation();
+
+    TEveGeoShape *clusterShape = new TEveGeoShape("cluster");
+    
+    clusterShape->SetMainColor(kRed);
+    clusterShape->SetMainTransparency(50);
         
     // Create the rotation matrix.
     TGeoRotation rot;
